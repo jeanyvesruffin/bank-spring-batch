d890906492c13dc1767b40e147244ecd490a1617
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-11-17 16:32:31 +0100

    init projet

---
+++ src/test/java/com/ruffin/bankspringbatch/BankSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffin.bankspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
