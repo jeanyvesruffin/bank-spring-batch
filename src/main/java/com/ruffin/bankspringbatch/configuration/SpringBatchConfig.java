e1d40ce2d65538ae6e8a75b0ccc6441a79a58fef
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-11-18 10:12:57 +0100

    ItemReader done

--- src/main/java/com/ruffin/bankspringbatch/configuration/SpringBatchConfig.java
+++ src/main/java/com/ruffin/bankspringbatch/configuration/SpringBatchConfig.java
@@ -1,13 +1,23 @@
 package com.ruffin.bankspringbatch.configuration;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.ruffin.bankspringbatch.dao.BankTransaction;
 
@@ -24,4 +34,37 @@ public class SpringBatchConfig {
 	private ItemWriter<BankTransaction> bankTransactionItemWriter;
 	@Autowired
 	private ItemProcessor<BankTransaction, BankTransaction> bankTransactionItemProcessor;
+
+	public Job bankJob() {
+		Step step1 = stepBuilderFactory.get("step-load-data").<BankTransaction, BankTransaction>chunk(100)
+				.reader(bankTransactionItemReader).processor(bankTransactionItemProcessor)
+				.writer(bankTransactionItemWriter).build();
+
+		return jobBuilderFactory.get("bank-data-load-job").start(step1).build();
+	};
+
+	@Bean
+	public FlatFileItemReader<BankTransaction> fileItemReader(@Value("${inputFile}") Resource inputFile) {
+		FlatFileItemReader<BankTransaction> fileItemReader = new FlatFileItemReader<>();
+		fileItemReader.setName("FFIR1");
+		fileItemReader.setLinesToSkip(1);
+		fileItemReader.setResource(inputFile);
+		fileItemReader.setLineMapper(lineMapper());
+		return fileItemReader;
+	}
+
+	@Bean
+	public LineMapper<BankTransaction> lineMapper() {
+		DefaultLineMapper<BankTransaction> lineMapper = new DefaultLineMapper<>();
+		DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+		delimitedLineTokenizer.setDelimiter(",");
+		delimitedLineTokenizer.setStrict(false);
+		delimitedLineTokenizer.setNames("id","accountID","strTransactionDate","transactionType","amount");
+		lineMapper.setLineTokenizer(delimitedLineTokenizer);
+		BeanWrapperFieldSetMapper<BankTransaction> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		fieldSetMapper.setTargetType(BankTransaction.class);
+		lineMapper.setFieldSetMapper(fieldSetMapper);
+		return lineMapper;
+	}
+
 }
